fa9389243ddee87d2a4d48505c31ad61039923d9
vanp1 <161830912+vanp1@users.noreply.github.com>
2024-04-03 13:16:05 +0700

    Add files via upload

---
+++ index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="header-container">
+        <TopCork></TopCork>
+    </div>
+    <div class="main-container" id="container">
+
+        <div class="overlay"></div>
+        <div class="search-overlay"></div>
+
+        <div class="topbar-nav header navbar" role="banner">
+            <MenuCork></MenuCork>
+
+        </div>
+
+        <div id="content" class="main-content">
+            <router-view></router-view>
+        </div>
+        <BotCork></BotCork>
+    </div>
+</template>
+<script>
+import TopCork from "../components/TopCork.vue";
+import BotCork from "../components/BotCork.vue";
+import MenuCork from "../components/MenuCork.vue";
+import "../../assets/js/libs/jquery-3.1.1.min.js";
+import "../../bootstrap/js/bootstrap.min.js";
+import "../../assets/js/app.js";
+
+export default {
+    name: "app",
+    components: {
+        TopCork, BotCork, MenuCork
+    },
+
+}
+</script>
+<style>
+@import"../../bootstrap/css/bootstrap.min.css";
+@import"../../assets/css/plugins.css";
+@import"../../plugins/apex/apexcharts.css";
+@import"../../assets/css/dashboard/dash_2.css";
+</style>
