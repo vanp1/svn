bd3f9b8bd791da933cfd0f86914e8cad3d63bdf1
vanp1 <161830912+vanp1@users.noreply.github.com>
2024-04-03 14:02:06 +0700

    Add files via upload

---
+++ số nguyên tố.java
@@ -0,0 +1,25 @@
+
+public class PrimeNumberCheck {
+
+    public static void main(String[] args) {
+        int num = 29;
+        boolean flag = isPrime(num);
+        if (flag) {
+            System.out.println(num + " là số nguyên tố");
+        } else {
+            System.out.println(num + " không phải là số nguyên tố");
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
